2560ee517457db442e9b37c9af6625651c7465ca
UserXGnu <user_x@riseup.net>
2017-10-24 18:25:08 -0300

    Adding build options

--- build.sh
+++ build.sh
@@ -1,2 +1,8 @@
 #!/bin/bash
-g++ -std=c++11 main.cpp -o hexdmp
+if [ -z $1 ]; then
+    g++ -std=c++11 main.cpp -o hexdmp
+elif [ $1 == "_DEBUG" ]; then
+    g++ -std=c++11 main.cpp -o hexdmp -D$1
+else
+    printf "$1 is not a valid option\nValid Options:\n\t_DEBUG\n"
+fi
